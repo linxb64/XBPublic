b88953122e7b2793de5b1c7ed36aa4101b9272f1
lxb <linxb64@163.com>
2018-05-08 01:46:15 +0800

    init code

---
+++ XBPublic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XBPublic
+//
+//  Created by 林晓兵 on 2018/5/7.
+//  Copyright © 2018年 XB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
